610abc91ee42c06c57fd05669ec63b81e77ccd18
FakaFakaYeah <fakafaka270695@yandex.ru>
2023-09-21 14:56:58 +0300

    update endpoints

--- app/crud/base.py
+++ app/crud/base.py
@@ -1,9 +1,9 @@
 from datetime import date
 
-from sqlalchemy import select, and_, delete
+from sqlalchemy import select, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.services.exceptions import ObjNotFound
+from app.api.exceptions import ObjNotFound
 from app.models import Bookings
 
 
--- app/crud/bookings.py
+++ app/crud/bookings.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud import CRUDBase
 from app.models import Bookings, Rooms
-from app.services.exceptions import RoomCannotBeBooked
+from app.api.exceptions import RoomCannotBeBooked
 
 
 class BookingCRUDBase(CRUDBase):
