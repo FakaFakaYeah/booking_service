b8d40ef46220c9a4d4a26a7f4b75ed885f0075f1
FakaFakaYeah <fakafaka270695@yandex.ru>
2023-09-21 14:04:18 +0300

    update endpoints

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import bookings_router, users_router, hotels_router
-from app.pages import pages_router
 
 main_router = APIRouter()
 main_router.include_router(users_router)
@@ -14,10 +13,4 @@ main_router.include_router(
     hotels_router,
     prefix='/hotels',
     tags=['Отели']
-)
-
-main_router.include_router(
-    pages_router,
-    prefix='/pages',
-    tags=['Фронтенд']
 )
