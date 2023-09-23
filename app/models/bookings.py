f2b84eaa9549fc90b8216ab3f0f353a0aa70470e
FakaFakaYeah <fakafaka270695@yandex.ru>
2023-09-24 00:57:51 +0300

    add admin panel

--- app/models/bookings.py
+++ app/models/bookings.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 from sqlalchemy import ForeignKey, Computed
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.core import Base
 
@@ -17,6 +17,7 @@ class Bookings(Base):
         Computed("(date_to - date_from) * price")
     )
     total_days: Mapped[int] = mapped_column(Computed("date_to - date_from"))
+    bookings = relationship("Users", back_populates='bookings', uselist=False)
 
     def __repr__(self):
         return f'Бронирование #{self.id} c {self.date_from} по {self.date_to}'
--- app/models/users.py
+++ app/models/users.py
@@ -6,7 +6,7 @@ from app.core import Base
 
 class Users(Base, SQLAlchemyBaseUserTable[int]):
 
-    bookings = relationship("Bookings", backref='user')
+    bookings = relationship("Bookings", back_populates='user')
 
     def __repr__(self):
         return f'Пользователь {self.email}'
