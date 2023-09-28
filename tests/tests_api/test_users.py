69952ccc825fa65cbd8745863abf6027f74ac484
FakaFakaYeah <fakafaka270695@yandex.ru>
2023-09-28 23:23:15 +0300

    add pytest

--- tests/tests_api/test_users.py
+++ tests/tests_api/test_users.py
@@ -1,19 +1,10 @@
-import pytest
 from httpx import AsyncClient
 
 
-@pytest.mark.parametrize('email, password, status_code', [
-    ('test@test.ru', '1234556', 201),
-    ('test@test.ru', '1234556', 400),
-    ('sdfsd', '1234556', 422),
-])
-async def test_register_user(
-    test_client: AsyncClient,
-    email, password, status_code
-):
+async def test_register_user(test_client: AsyncClient):
 
     response = await test_client.post(
-        'auth/register', json=dict(email=email, password=password)
+        'auth/register', json=dict(email='test@test.ru', password='2354656')
     )
-    assert response.status_code == status_code
+    assert response.status_code == 201
     print(response.status_code)
