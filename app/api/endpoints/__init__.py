5781a919692f5ae1e0edf07d53c15b2643eaa019
FakaFakaYeah <fakafaka270695@yandex.ru>
2023-10-04 00:07:05 +0300

    add rooms router

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -1,3 +1,4 @@
 from app.api.endpoints.bookings import router as bookings_router # noqa
 from app.api.endpoints.users import router as users_router # noqa
 from app.api.endpoints.hotels import router as hotels_router # noqa
+from app.api.endpoints.rooms import router as rooms_router # noqa
--- app/api/routers.py
+++ app/api/routers.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import bookings_router, users_router, hotels_router
+from app.api.endpoints import (
+    bookings_router, users_router, hotels_router, rooms_router
+)
 
 main_router = APIRouter()
 main_router.include_router(users_router)
@@ -13,4 +15,10 @@ main_router.include_router(
     hotels_router,
     prefix='/hotels',
     tags=['Отели']
+)
+
+main_router.include_router(
+    rooms_router,
+    prefix='/rooms',
+    tags=['Номера']
 )
