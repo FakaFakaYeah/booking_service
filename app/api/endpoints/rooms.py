72dff896a44436e84c714500359f8ca48fab0f81
FakaFakaYeah <fakafaka270695@yandex.ru>
2023-10-04 00:49:59 +0300

    add rooms endpoints

--- app/api/endpoints/rooms.py
+++ app/api/endpoints/rooms.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core import get_async_session
-from app.schemas.rooms import RoomDB
-from app.crud import RoomsCrud
+from app.schemas.rooms import RoomDB, RoomsCreate
+from app.crud import RoomsCrud, HotelsCrud
+from app.core.users import current_superuser
 
 
 router = APIRouter()
@@ -19,3 +20,18 @@ async def get_rooms(
         session: AsyncSession = Depends(get_async_session)
 ):
     return await RoomsCrud.get_by_id(obj_id=room_id, session=session)
+
+
+@router.post(
+    '/',
+    summary='Добавление нового номера',
+    response_model=RoomDB,
+    dependencies=[Depends(current_superuser)],
+    description='Добавление новой комнаты доступно только админу'
+)
+async def room_create(
+        room: RoomsCreate,
+        session: AsyncSession = Depends(get_async_session)
+):
+    await HotelsCrud.get_by_id(obj_id=room.hotel_id, session=session)
+    return await RoomsCrud.create(obj_in=room, session=session)
--- app/schemas/rooms.py
+++ app/schemas/rooms.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, Extra, PositiveInt
 
 
 class RoomDB(BaseModel):
@@ -16,3 +18,28 @@ class RoomsPriceDB(RoomDB):
 
     rooms_left: int
     total_coast: int
+
+
+class RoomsCreate(BaseModel):
+
+    hotel_id: PositiveInt
+    name: str
+    description: str
+    price: PositiveInt
+    services: list[str]
+    quantity: PositiveInt
+    image_id: PositiveInt
+
+    class Config:
+        extra = Extra.forbid
+        json_schema_extra = {
+            'example': {
+                'hotel_id': 1,
+                'name': 'Название номера',
+                'description': 'Описание номера',
+                'price': 1000,
+                'services': ['Сервисы номера'],
+                'quantity': 10,
+                'image_id': 1
+            }
+        }
